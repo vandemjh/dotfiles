b70b3f22ac06b0efe12520982bd09b80af9e3533
vandemjh <jack@jackv.dev>
2023-03-10 12:08:07 -0500

    add miniconda

--- .shells/exports.sh
+++ .shells/exports.sh
@@ -20,6 +20,21 @@ export LANG=C.UTF-8 # or any UTF-8 English locale supported by your OS may be us
 
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/`whoami`/.miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/`whoami`/.miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/`whoami`/.miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/`whoami`/.miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
 # Adds inputrc file from home
 # export INPUTRC=~/.inputrc
 # source ~/.inputrc
